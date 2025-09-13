fd6156180d6eddb62f443dc58c1fb4856fb25701
Ian Meinert <marinesct@gmail.com>
2025-09-12 21:20:03 -0500

    added virtual keyboard support

--- scripts/start-kiosk.sh
+++ scripts/start-kiosk.sh
@@ -4,13 +4,14 @@
 # Wait for services to be ready
 sleep 10
 
+# Enable on-screen keyboard first
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+
 # Start Chromium in kiosk mode
 exec chromium-browser \
     --kiosk \
+    --enable-virtual-keyboard \
+    --app=http://localhost:5173 \
     --no-first-run \
-    --disable-infobars \
-    --disable-session-crashed-bubble \
-    --disable-translate \
-    --disable-features=VizDisplayCompositor \
-    --start-fullscreen \
-    --app=http://localhost:5173
+    --touch-events=enabled
